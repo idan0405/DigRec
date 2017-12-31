bf444deee87a84382d6fcd13c73adde6a40b7fd2
idan glassberg <idanglass0405@gmail.com>
2017-12-31 22:15:58 +0200

    better accuracy
    0.99985

--- deeplearning/letsgo.py
+++ deeplearning/letsgo.py
@@ -13,24 +13,14 @@ config = tf.ConfigProto(intra_op_parallelism_threads=4,\
         device_count = {'CPU' : 1, 'GPU' : 1})
 session = tf.Session(config=config)
 K.set_session(session)
-#mini batch gradient descent ftw
 batch_size = 128
-#10 difference characters
 num_classes = 10
-#very short training time
 epochs = 30
 
-# input image dimensions
-#28x28 pixel images. 
 img_rows, img_cols = 28, 28
-
-# the data downloaded, shuffled and split between train and test sets
-#if only all datasets were this easy to import and format
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 print(x_train.shape[0], 'train samples')
-#this assumes our data format
-#For 3D data, "channels_last" assumes (conv_dim1, conv_dim2, conv_dim3, channels) while 
-#"channels_first" assumes (channels, conv_dim1, conv_dim2, conv_dim3).
+
 if K.image_data_format() == 'channels_first':
     x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
     x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
@@ -42,7 +32,6 @@ else:
     input_shape = (img_rows, img_cols, 1)
     
 
-#more reshaping
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 x_train /= 255
@@ -51,47 +40,33 @@ print('x_train shape:', x_train.shape)
 print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
 
-# convert class vectors to binary class matrices
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
-#build our model
 model = Sequential()
-#convolutional layer with rectified linear unit activation
-model.add(Conv2D(32, kernel_size=(5, 5),
+model.add(Conv2D(64, kernel_size=(5, 5),
                  activation='relu',
                  input_shape=input_shape))
-#again
-model.add(Conv2D(64, (5, 5), activation='relu'))
-#choose the best features via pooling
-model.add(MaxPooling2D(pool_size=(3, 3)))
-#randomly turn neurons on and off to improve convergence
+model.add(Conv2D(128, (5, 5), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
-#flatten since too many dimensions, we only want a classification output
 model.add(Flatten())
-#fully connected to get all relevant data
-model.add(Dense(128, activation='relu'))
-#one more dropout for convergence' sake :) 
+model.add(Dense(1024, activation='relu'))
 model.add(Dropout(0.5))
-#output a softmax to squash the matrix into output probabilities
 model.add(Dense(num_classes, activation='softmax'))
-#Adaptive learning rate (adaDelta) is a popular form of gradient descent rivaled only by adam and adagrad
-#categorical ce since we have multiple classes (10) 
 model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.Adadelta(),
               metrics=['accuracy'])
-newxs=np.array(pd.read_csv('train.csv').drop(['label'],1))
 
+newxs=np.array(pd.read_csv('train.csv').drop(['label'],1))
 newys= np.array(pd.read_csv('train.csv')['label'])
 
-       
-#train that ish!
 model.fit(x_train, y_train,
           batch_size=batch_size,
           epochs=epochs,
           verbose=2,
           )
- #how well did it do? 
+
 model.fit(x_test, y_test,
           batch_size=batch_size,
           epochs=epochs,
